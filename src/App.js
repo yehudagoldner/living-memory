da23eeecdcec3527e85f816d1b19d73a67263bf1
KeterPerry <keterav@gmail.com>
2022-08-10 14:10:09 +0300

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-import Navbar from "./components/Navbar/Navbar.js";
 import FriendList from "./pages/friendList/friendList.jsx";
 import Contribute from "./pages/contribute/contribute.jsx";
 import UploadFile from "./pages/upload file/uploadFile.jsx";
-
 import "./App.css";
-// import ArrowDown from "./components/arrowDown/arrowDown.jsx";
-=======
 import React from "react";
 import Navbar from "../src/components/Navbar/Navbar";
 import Header from "../src/components/Header/Header";
 import Email from "../src/components/Email/Email";
 import PopUp from "../src/components/PopUp/PopUp";
 import HomePage from "../src/pages/homePage/HomePage";
-// import Main from "./pages/friend list/Main";
 import Footer from "./components/Footer/Footer";
 import DrawerAppBar from "./components/Navbar/Navbar.js";
 import {
@@ -29,20 +23,17 @@ import "./App.css";
 import TermsOfUse from "./components/TermsOfUse/TermsOfUse";
 // import AddPhoto from "./pages/homePage/linkComponents/addingPhoto/AddPhoto";
 // import AddWriting from "./pages/homePage/linkComponents/addingWriting/AddWriting";
->>>>>>> 27afc70f38c3fbd21897ae3adbc64fc6fbdf0560
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />;{/* <FriendList /> */}
+      <Navbar />;
+      <Header />
+      {/* <FriendList /> */}
       {/* <Contribute /> */}
       <UploadFile />
-=======
       <BrowserRouter>
         <Navbar />
-        <Header />
-
         <Switch>
           <Route exact path="/details" component={PopUp} />
 
@@ -61,7 +52,6 @@ function App() {
         </Switch>
         <Footer />
       </BrowserRouter>
->>>>>>> 27afc70f38c3fbd21897ae3adbc64fc6fbdf0560
     </div>
   );
 }
