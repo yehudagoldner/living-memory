5d77104ecbd59e12c7bcf79cd4ab5ac018345c4b
“yehudagoldner” <goldnery@gmail.com>
2022-09-04 15:43:59 +0300

    fixing bug

--- controllers/generic.js
+++ controllers/generic.js
@@ -1,50 +1,66 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const get = async (req, res) => {
+  try {
+    if (req.params.id) {
+      return res.json(
+        await prisma[req.params.entity].findFirst({
+          where: { id: req.params.id * 1 },
+        })
+      );
+    }
 
-const get = async (req, res) => {   
-   try {
-       if (req.params.id) {
-         return res.json(
-           await prisma[req.params.entity].findFirst({ where: { id: req.params.id*1 } })
-         );
-       }
+    console.log(req.params.userId);
 
-       const user = await prisma.user.findFirst({ where: { id: req.params.userId*1 } })
-       return res.json(await prisma[req.params.entity].findMany({ where: { userId: user.facebook_id } }));
-   } catch(e){
+    const userId =
+      req.params.entity.toLowerCase() === "candles"
+        ? req.params.userId
+        : await prisma.user.findFirst({ where: { id: req.params.userId } })
+            .facebook_id;
+
+    return res.json(
+      await prisma[req.params.entity].findMany({
+        where: { userId },
+      })
+    );
+  } catch (e) {
     return res.json(e);
-   }
+  }
 };
 
 const create = async (req, res) => {
-  try{
-    console.log(req.body);  
-    let record = {    
-        ...req.body
-    }
-         
+  try {
+    console.log(req.body);
+    let record = {
+      ...req.body,
+    };
+
     console.log(record);
     res.json(
-        await prisma[req.params.entity].upsert({
-        where: { id: req.params.id? req.params.id*1: -1 },
+      await prisma[req.params.entity].upsert({
+        where: { id: req.params.id ? req.params.id * 1 : -1 },
         create: record,
         update: record,
-        })
+      })
     );
-  } catch(e){
+  } catch (e) {
     return res.json(e);
-   }
+  }
 };
 
 const updatePost = async (req, res) => {};
 
 const del = async (req, res) => {
-try {
-  res.json(await prisma[req.params.entity].delete({ where: { id: req.params.id*1 } }));
-} catch(e){
+  try {
+    res.json(
+      await prisma[req.params.entity].delete({
+        where: { id: req.params.id * 1 },
+      })
+    );
+  } catch (e) {
     return res.json(e);
-   }
+  }
 };
 
 module.exports = { get, create, del };
