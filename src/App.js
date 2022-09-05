d13ccc1a436d861e213c9e3d8e29e1cddad090c7
hibara89 <hiba.rashed@gmail.com>
2022-09-05 12:13:26 +0300

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@
 import "./App.css";
 import React from "react";
 import Wizard from "../src/components/Wizard/Wizard";
-import {
-  Switch,
-  Route,  
-} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { FacebookProvider } from "./context/context.provider.jsx";
 
 import "./App.css";
@@ -15,33 +12,24 @@ import UserRoutes from "./UserRoutes.js";
 // import AddPhoto from "./pages/homePage/linkComponents/addingPhoto/AddPhoto";
 // import AddWriting from "./pages/homePage/linkComponents/addingWriting/AddWriting";
 
-
 function App() {
   return (
-<<<<<<< HEAD
-    <FacebookProvider>
-      <div className="App">
-        <Navbar />
-        {/* <Header /> */}
-=======
     <div className="App">
-    
->>>>>>> 0578df523ac7e05b90b534b824cedf5a3a8cb930
-        {/* <FriendList /> */}
-        {/* <Contribute /> */}
-        {/* <UploadFile /> */}
-        {/* <BrowserRouter> */}
-        {/* <Navbar /> */}
+      {/* <FriendList /> */}
+      {/* <Contribute /> */}
+      {/* <UploadFile /> */}
+      {/* <BrowserRouter> */}
+      {/* <Navbar /> */}
 
-        <Switch>
-          <Route exact path="/" component={Wizard} />
-          
-            {/* <Route exact path="/bookview" component={Book} /> */}
-            <Route exact path="/:userid/" component={UserRoutes} />                      
-        </Switch>
-        {/* <Footer /> */}
-        {/* </BrowserRouter> */}
-      </div>
+      <Switch>
+        <Route exact path="/" component={Wizard} />
+
+        {/* <Route exact path="/bookview" component={Book} /> */}
+        <Route exact path="/:userid/" component={UserRoutes} />
+      </Switch>
+      {/* <Footer /> */}
+      {/* </BrowserRouter> */}
+    </div>
   );
 }
 
