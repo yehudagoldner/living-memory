298bec92ed293f8f724250c85c1c4c319b59cbb6
ShiraOhana <soshiraohana@gmail.com>
2022-09-17 14:56:46 +0300

    Section three data display

--- src/pages/homePage/components/sectionTwo/components/LeftSide.jsx
+++ src/pages/homePage/components/sectionTwo/components/LeftSide.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 
 import "./left.css";
-import AddView from "../../reusable/AddView";
 import { facebookContentContext } from "../../../../../context/context.provider";
 
 const LeftSide = () => {
--- src/pages/homePage/components/sectionTwo/components/RightSide.jsx
+++ src/pages/homePage/components/sectionTwo/components/RightSide.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import "./right.css";
-import Button from "../../reusable/Button";
-import Memorial from "../../reusable/Memorial";
-import View from "../../reusable/View";
+
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import AddView from "../../reusable/AddView";
@@ -12,8 +10,30 @@ const RightSide = () => {
   const [data, setData] = useContext(facebookContentContext);
 
   const [image, setImage] = useState(
+    "https://media.istockphoto.com/photos/burning-candle-on-black-table-memory-day-picture-id1314494173?k=20&m=1314494173&s=612x612&w=0&h=Ec3ZPR483Chr5GpEoo85wjGJbF5z5D4cC1v4wgbQO-A="
+  );
+  const [post, setPost] = useState(
     "Never sing for my supper, I never help my neighbour, Never do what is proper, For my share of labour, I'm a poor boy And I'm a rover, Count your coins and Throw them over them over the- Throw them over them over the Throw them."
   );
+
+  useEffect(() => {
+    if (data.posts) {
+      let randomIndex =
+        data.posts[Math.floor(Math.random() * data.posts.length)];
+      const currentImage = randomIndex.image;
+      setImage(currentImage);
+      console.log("currentImage", currentImage);
+    }
+  }, [data]);
+  useEffect(() => {
+    if (data.posts) {
+      let randomIndex =
+        data.posts[Math.floor(Math.random() * data.posts.length)];
+      const currentPost = randomIndex.text;
+      setPost(currentPost);
+    }
+  }, [data]);
+
   return (
     <div className="rightBox-wrapper">
       <div className="title-right">
@@ -21,12 +41,8 @@ const RightSide = () => {
         <h3>Some pictures he took</h3>
       </div>
       <div className="text-right">
-        <img src="Picture-section2.png" alt="" />
-        <p>
-          photo description: date, place, whos in it and more. Photo
-          de-scription: date, place, whos in it and more.Photo de-scription:
-          date, place..
-        </p>
+        <img src={image} alt="" />
+        <p>{post}</p>
       </div>
     </div>
   );
