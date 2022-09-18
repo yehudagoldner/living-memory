0aa46d8a5b8e7d9543234e4af7cce8d54f50f467
“yehudagoldner” <goldnery@gmail.com>
2022-09-18 15:24:27 +0300

    fix naming and db types

--- services/facebook/utils.js
+++ services/facebook/utils.js
@@ -31,7 +31,7 @@ const saveUser = async (user, cookie) => {
       lastName: cookie.lastName,
       password: md5(cookie.password),
       email: cookie.email,
-      user: user.facebook_id,
+      facebook_id: user.facebook_id,
     },
   });
   user.creator = creator.id
