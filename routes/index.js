b763ac2b8325648a4cd6807a1102b018bf6dd661
“yehudagoldner” <goldnery@gmail.com>
2022-09-18 15:15:49 +0300

    fix user auth support

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
-const postsRoutes = require("./posts")
-const genericRoute = require("./generic")
-const md5 = require('js-md5');
+const postsRoutes = require("./posts");
+const genericRoute = require("./generic");
+const md5 = require("js-md5");
 
 const { getQuery } = require("../services/getQuery");
 const app = express.Router();
@@ -10,30 +10,49 @@ const { collectFacebookData, login } = require("../services/facebook/");
 const prisma = new PrismaClient();
 app.get("/get_facebook_data", login);
 app.get("/save_facebook_data", collectFacebookData);
-app.use('/posts', postsRoutes)
-
+app.use("/posts", postsRoutes);
 
 // app.use('/api', genericRoute)
 
-app.route("/api/pagesLiked").get(async (req, res)=> res.json(await prisma.pagesLiked.findMany({})))
-app.route("/api/profileImages").get(async (req, res)=> res.json(await prisma.profileImages.findMany({})))
-app.route("/api/creator").get(async (req, res)=> res.json(await prisma.creator.findFirst({where:{email:req.query.email, password:req.query.password}})))
-app.get("/api/:userId/all", async (req, res) => {  
-  const user = await prisma.user.findFirst({ where: { facebook_id: req.params.userId } })
-  res.json( {
-   posts: await prisma.post.findMany({ where: { userId: user.id } }),
-   pagesLiked: await prisma.pagesLiked.findMany({ where: { userId: user.id } }),
-   profileImages: await prisma.profileImages.findMany({ where: { userId: user.id,} }),   
-  })
+app
+  .route("/api/pagesLiked")
+  .get(async (req, res) => res.json(await prisma.pagesLiked.findMany({})));
+app
+  .route("/api/profileImages")
+  .get(async (req, res) => res.json(await prisma.profileImages.findMany({})));
+app
+  .route("/api/creator")
+  .get(async (req, res) =>
+    res.json(
+      await prisma.creator.findFirst({
+        where: { email: req.query.email, password: md5(req.query.password) },
+      })
+    )
+  );
+app.get("/api/:userId/all", async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: { facebook_id: req.params.userId },
+  });
+  res.json({
+    posts: await prisma.post.findMany({ where: { userId: user.id } }),
+    pagesLiked: await prisma.pagesLiked.findMany({
+      where: { userId: user.id },
+    }),
+    profileImages: await prisma.profileImages.findMany({
+      where: { userId: user.id },
+    }),
+  });
 });
-app.use('/api/:userId', genericRoute)
+app.use("/api/:userId", genericRoute);
 app.get("/all", async (req, res) => {
-  res.json( {
-   posts: await prisma.post.findMany({}),
-   pagesLiked: await prisma.pagesLiked.findMany({}),
-   profileImages: await prisma.profileImages.findMany({}),
-   user: await prisma.user.findFirst({}),
-  })
+  res.json({
+    posts: await prisma.post.findMany({}),
+    pagesLiked: await prisma.pagesLiked.findMany({}),
+    profileImages: await prisma.profileImages.findMany({}),
+    user: await prisma.user.findFirst({}),
+  });
 });
+
+
 
 module.exports = app;
